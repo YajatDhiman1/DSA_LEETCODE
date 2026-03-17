0af53061b53ee284cd0e9fe2db3cd41b17f5d61d
Yajat1853 <yajat1853.becse24@chitkara.edu.in>
2026-03-17 23:30:33 +0530

    DSA JAVA

--- GFG_Ques/gcd.java
+++ GFG_Ques/gcd.java
@@ -1,5 +1,23 @@
 package GFG_Ques;
-
 public class gcd {
-  
+  static boolean armstrongNumber(int n) {
+    int h=n;
+    int len=0;
+    int sum=0;
+    while(h != 0){
+      h /= 10;
+      len++;
+    }
+    h=n;
+    while(h != 0){
+      int temp=h%10;
+      sum += (int) Math.pow(temp, len);
+      h /= 10;
+    }
+    if(sum == n){
+      return true;
+    }else{
+      return false;
+    }
+  }
 }

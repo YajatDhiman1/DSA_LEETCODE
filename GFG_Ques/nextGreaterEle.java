b93756aca739f683f916ec93ae88c16f43015a36
Yajat Dhiman <yajatdhiman@gmail.com>
2026-03-23 22:45:11 +0530

    DSA JAVA

---
+++ GFG_Ques/nextGreaterEle.java
@@ -0,0 +1,27 @@
+package GFG_Ques;
+import java.util.*;
+
+class Solution {
+  public ArrayList<Integer> nextLargerElement(int[] arr) {
+    ArrayList<Integer> res = new ArrayList<>();
+    Stack<Integer> stk=new Stack<>();
+        
+    for(int x: arr){
+      res.add(-1);
+    }
+        
+    int n=arr.length;
+        
+    for(int i=n-1; i>=0; i--){
+      while(!stk.isEmpty() && stk.peek() <= arr[i]){
+        stk.pop();
+      }
+      if(!stk.isEmpty()) {
+        res.set(i,stk.peek());
+      }
+      stk.push(arr[i]);
+    }
+        
+    return res;
+    }
+}

f04597b83a64f669267c3528733fd8d88ab13dd3
Yajat Dhiman <yajatdhiman@gmail.com>
2026-03-25 23:20:06 +0530

    DSA JAVA

---
+++ GFG_Ques/stockSpanProblem.java
@@ -0,0 +1,27 @@
+package GFG_Ques;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class stockSpanProblem {
+  public ArrayList<Integer> calculateSpan(int[] arr) {
+        ArrayList<Integer> res = new ArrayList<>();
+        Stack<Integer> st = new Stack<>();
+        
+        for(int i = 0; i < arr.length; i++){
+            while(!st.isEmpty() && arr[st.peek()] <= arr[i]){
+                st.pop();
+            }
+            
+            if(st.isEmpty()){
+                res.add(i + 1);
+            } else {
+                res.add(i - st.peek());
+            }
+            
+            st.push(i);
+        }
+        
+        return res;
+    }
+}

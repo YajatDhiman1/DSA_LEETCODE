2cfb06c75bce8a41830a976bbca3f424bff04bf3
Yajat Dhiman <yajatdhiman@gmail.com>
2026-03-29 23:59:16 +0530

    DSA JAVA

---
+++ GFG_Ques/SortLL.java
@@ -0,0 +1,31 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}*/
+import java.util.*;
+class SortLL {
+    public Node segregate(Node head) {
+        Node hh=head;
+        ArrayList<Integer> list=new ArrayList<>();
+        while(hh!=null){
+            list.add(hh.data);
+            hh=hh.next;
+        }
+        Collections.sort(list);
+        Node gg=new Node(list.get(0));
+        Node rev=gg;
+        for(int i=1; i<list.size(); i++){
+            Node ff=new Node(list.get(i));
+            rev.next=ff;
+            rev=ff;
+        }
+        return gg;
+    }
+}

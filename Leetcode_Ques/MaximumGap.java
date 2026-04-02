a88858149d810e4568ba3f415211238541a2b4d6
Yajat Dhiman <yajatdhiman@gmail.com>
2026-04-02 23:20:10 +0530

    DSA JAVA

---
+++ Leetcode_Ques/MaximumGap.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        Arrays.sort(nums);
+        int max=0;
+        int temp=0;if(nums.length<2)return 0;
+        for(int i=0; i<nums.length-1; i++){
+            temp = nums[i+1]-nums[i];
+            if(temp > max){
+                max = temp;
+            }
+        }
+       return max;
+    }
+}

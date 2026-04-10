075457ef94e137bdca5b432c52342c453748c3e9
Yajat Dhiman <yajatdhiman@gmail.com>
2026-04-10 19:58:03 +0530

    DSA JAVA

---
+++ Leetcode_Ques/MinimumDistance.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minimumDistance(int[] nums) {
+        if (nums.length < 3) {
+            return -1;
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] == nums[j] && nums[j] == nums[k]) {
+                        int distance = calculateDistance(i, j, k);
+                        min = Math.min(min, distance);
+                    }
+                }
+            }
+        }
+
+        if(min==Integer.MAX_VALUE){
+            return -1;
+        }
+        return min;
+    }
+
+    private int calculateDistance(int i, int j, int k) {
+        return Math.abs(i - j)
+             + Math.abs(j - k)
+             + Math.abs(k - i);
+    }
+}

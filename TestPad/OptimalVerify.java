e7e4feff36070be336cf8a730f552de92801f9af
Yajat Dhiman <yajatdhiman@gmail.com>
2026-04-14 19:56:19 +0530

    DSA JAVA

---
+++ TestPad/OptimalVerify.java
@@ -0,0 +1,9 @@
+static boolean verifyPrime(int n) {
+    if(n==1 || n==0)return false;
+    for(int i=2; i*i<=n; i++){
+        if(n % i==0){
+            return false;
+        }
+    }
+    return true;
+}

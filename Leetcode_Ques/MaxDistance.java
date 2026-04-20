073efa286e71de1e7d0b50d52ed65f1aa59dd931
Yajat Dhiman <yajatdhiman@gmail.com>
2026-04-20 12:42:03 +0530

    DSA JAVA

---
+++ Leetcode_Ques/MaxDistance.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int maximumDifference(int[] nums) {
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] < nums[j]) {
+                    int dis = nums[j] - nums[i];
+                    max = Math.max(dis, max);
+                }
+            }
+        }
+        if (max == 0) {
+            return -1;
+        }
+        return max;
+    }
+}

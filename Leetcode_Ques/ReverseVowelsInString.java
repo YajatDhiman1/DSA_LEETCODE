bfc400490d58a041e1b581e9075a0544db7561f2
Yajat Dhiman <yajatdhiman@gmail.com>
2026-05-02 23:52:40 +0530

    DSA JAVA

---
+++ Leetcode_Ques/ReverseVowelsInString.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String reverseVowels(String s) {
+        char[] arr = s.toCharArray();
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            if (!isVowel(arr[left])) {
+                left++;
+            } else if (!isVowel(arr[right])) {
+                right--;
+            } else {
+                // swap
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        return new String(arr);
+    }
+
+    private boolean isVowel(char c) {
+        c = Character.toLowerCase(c);
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+}

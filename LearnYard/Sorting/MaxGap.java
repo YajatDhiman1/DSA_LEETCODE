fc814b512b29d095ce671fe078edbeeb1767ebed
YajatDhiman1 <yajatdhiman@gmail.com>
2026-09-01 23:53:55 +0530

    DSA JAVA

---
+++ LearnYard/Sorting/MaxGap.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        Arrays.sort(nums);
+        int max=0;
+        if(nums.length < 2){
+            return max;
+        }else{
+            for(int i=0; i<nums.length-1; i++){
+                max=Math.max(nums[i+1]-nums[i], max);
+            }
+            return max;
+        }       
+    }
+}

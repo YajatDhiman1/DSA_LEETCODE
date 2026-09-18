76fea0126d0fdc0e50755ea59d00da377106e533
YajatDhiman1 <yajatdhiman@gmail.com>
2026-09-18 16:46:38 +0530

    DSA JAVA

--- TestPad/Datechk.java
+++ TestPad/Datechk.java
@@ -1,3 +1,19 @@
+import java.util.*;
+import java.time.LocalDate;
+
+
 public class Datechk {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LocalDate checkin = LocalDate.parse(sc.next());
+        int total = sc.nextInt();
+        int lived = sc.nextInt();
+        LocalDate checkout = checkin.plusDays(total);
+        int remaining = total - lived;
+
+        System.out.println(checkout);
+        System.out.println(remaining);
+        System.out.println(checkout.getYear());
+        System.out.println(checkout.getMonthValue());
+    }
 }
